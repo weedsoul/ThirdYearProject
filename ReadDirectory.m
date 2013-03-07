ab1745c64d4b3a6651e10340f927a9dc08bd0ba7
Paul Nunnerley <mail@paulnunnerley.com>
2013-03-07 00:51:50 +0000

    copied file from fach which reads a directory of wav files into a cell array

---
+++ ReadDirectory.m
@@ -0,0 +1,13 @@
+%This method reads all the files in a directory of wav files and writes the
+%results into a cell array which will be essentailly an array of wav files
+%ready to be processed by a feature extraction algorithm
+function [data, fss] = ReadDirectory(cdir)
+    Files=dir(cdir);
+    %define the size of the cell array to store the files
+    for k = 1:length(Files)
+        fname=fullfile(fileparts(cdir), Files(k).name);
+        [wave, fs] = wavread(fname);
+        data{k} = wave;
+        fss(k) = fs;
+    end
+end 
